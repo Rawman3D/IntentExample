f72c2fa418c9d1c3327733a1fab5cc4ea29f0624
Ramendra Manandhar <ramendramanandhar@lftechnology.com>
2016-06-21 13:26:57 +0545

    Initial commit..

---
+++ app/src/main/java/com/example/sayami/intentexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sayami.intentexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void openBacon(View view){
+        Intent i= new Intent(this,bacon_activity.class);
+
+        TextView appleInput = (TextView) findViewById(R.id.ApplesInput);
+
+        String userMessage= appleInput.getText().toString();
+
+        i.putExtra("ApplesMessage",userMessage);
+
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/sayami/intentexample/bacon_activity.java
@@ -0,0 +1,37 @@
+package com.example.sayami.intentexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class bacon_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacon_activity);
+
+
+        Bundle applesData = getIntent().getExtras();
+         if(applesData==null){
+             return;
+         }
+
+        String applesMessage = applesData.getString("ApplesMessage");
+
+        final TextView baconText = (TextView) findViewById(R.id.bacon_text);
+        baconText.setText(applesMessage);
+
+
+    }
+
+    public void callApple(View view){
+        // Intent code to launch a new activity.
+
+        Intent i = new Intent(this, MainActivity.class);
+
+        startActivity(i);
+    }
+}
